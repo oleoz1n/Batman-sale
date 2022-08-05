c6d36c1dde2b2aa8b7acd6a521ec4143926ce260
oleoz1n <103969568+oleoz1n@users.noreply.github.com>
2022-08-05 19:18:23 -0300

    1233
    123

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let soma = document.getElementById('soma');
+let close = document.getElementsByClassName('close');
+let home = document.getElementById('sa1')
+let video = document.getElementById('sa2')
+let sinopse = document.getElementById('sa3')
+
+function sa2() {
+    soma.style.display = "flex";
+}
+
